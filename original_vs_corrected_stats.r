c8a0bd59215d493192cd997370c2176547c6954c
cesine <cesine@yahoo.com>
2012-05-18 15:08:40 -0400

    adding sample r code

---
+++ original_vs_corrected_stats.r
@@ -0,0 +1,17 @@
+#read in data
+syllables <- read.table("original_vs_corrected.csv", header=TRUE, sep="," )
+t.test(syllables$original,syllables$corrected,paired=TRUE, alt="less")
+
+
+#	Paired t-test
+
+#data:  syllables$original and syllables$draft2 
+#t = -0.6826, df = 4, p-value = 0.2662
+#alternative hypothesis: true difference in means is less than 0 
+#95 percent confidence interval:
+#      -Inf 0.3057439 
+#sample estimates:
+#mean of the differences 
+#                 -0.144 
+
+
